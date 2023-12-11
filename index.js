2522bbeb108641c881b82814fa54921f32ab5b6e
tnvbsi <81610081+tnvbsi@users.noreply.github.com>
2023-12-11 11:52:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-var readline = require('readline');
-const clipboardy = require('clipboardy');
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-var issuccess=true;
-function s(t){
-	console.log("["+require("chalk").green.bold("InvisibleMessage")+"]: "+t)
+const readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout),
+    clipboardy = require("clipboardy"),
+    chalk = require("chalk");
+    
+function s(t) {
+    console.log(`[${chalk.green.bold("InvisibleMessage")}]: ${t}`);
 }
-function e(t){
-	console.log("["+require("chalk").red.bold("InvisibleMessage")+"]: "+t)
+
+function e(t) {
+    console.log(`[${chalk.red.bold("InvisibleMessage")}]: ${t}`);
 }
+
 console.log(`Example: \"<https://www.roblox.com>(nl)https://www.youtube.com\"`);
-function Q(){
-rl.question("> ",function(aboba) {
-if(!aboba.includes("(nl)")){e("This message is not valid.");issuccess=false;}
-var split=aboba.split("(nl)");
-if(split.length >2 || split[0] == null || split[0] == undefined || split[1] == null || split[1] == undefined || split[1].toString()==""){if(issuccess==true){e("Incorrect Usage!");issuccess=false;};}
-var content=`${split[0]}** **||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||||||||||${split[1]}`;
-try{
-clipboardy.writeSync(content);
-}catch(err){
-	issuccess=false;
-	e(err);
-}
-if(issuccess == true){
-s("Success!");
-}else{
-	issuccess=true;
-}
-Q();
+rl.setPrompt('> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+     let data = line.trim();
+     if (!data.includes("(nl)")) {
+        return e("This message is not valid.");
+     }
+     var split = data.split("(nl)");
+     if (split.length > 2 || split[0] == null || split[0] == undefined || split[1] == null || split[1] == undefined || split[1].toString() == "") {
+         return e("Incorrect Usage!");
+     }
+     var content = `${split[0]}** **||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||||||||||${split[1]}`;
+     clipboardy.write(content).then(() => {
+         return s("Success! Copied to clipboard.");
+     }).catch((err) => {
+         return e(err.toString());
+     });
 })
-}
-Q();
